50ab0a1e2326ce710f16bc9c71088083541469d4
sseungbin <smiles9501@naver.com>
2023-09-14 20:27:35 +0900

    승빈받음

--- src/main/java/org/project/controller/PageController.java
+++ src/main/java/org/project/controller/PageController.java
@@ -31,11 +31,10 @@ public class PageController {
 	@Autowired
 	private MemberService memberservice;
 	@Autowired
-<<<<<<< HEAD
+
 	private ContentsService contentsservice;
-=======
+
 	private MusicalService musicalservice;
->>>>>>> 49cfba42366ffb15f83a8cc5282d8d647f3c673d
 	
 	@GetMapping("/calendar")
 	public String calendar(Model model, HttpServletRequest request, DateData dateData) {
